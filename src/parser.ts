6a16b6cdab397937c85919d70bfdb75eacf0a817
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-06-12 14:49:09 +0200

    add poll data parser

---
+++ src/parser.ts
@@ -0,0 +1,31 @@
+import { PollData } from './interfaces/poll-data';
+import { Option } from './option';
+import { Participant } from './participant';
+import { Poll } from './poll';
+
+export function parsePollData(pollData: PollData): Poll {
+  const options: Option[] = [];
+  const participants: Participant[] = [];
+
+  for (let optionText of pollData.optionsText) {
+    options.push(new Option(optionText));
+  }
+
+  for (let participantData of pollData.participants) {
+    const participant = new Participant(participantData.name, participantData.locked);
+    participant.options = participantData.preferences
+      .split('')
+      .map((yesNo, index) => ({ selected: yesNo === 'y', index }))
+      .filter(option => option.selected)
+      .map(option => option.index)
+      .map(index => options[index]);
+
+    participant.options.forEach(option => {
+      option.participants.push(participant);
+    });
+
+    participants.push(participant);
+  }
+
+  return new Poll(options, participants);
+}
