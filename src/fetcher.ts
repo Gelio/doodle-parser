92239192513fff23dc4dd60c0bfc1bff86e5927c
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2017-06-12 15:50:06 +0200

    use node-fetch in fetcher, add optional request headers

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -1,19 +1,28 @@
 import { PollData } from './interfaces/poll-data';
+import * as fetch from 'node-fetch';
 
 const pollDataRegex = /\$\.extend\(true, doodleJS\.data, (.*)\);/g;
 const baseDoodleUrl = 'http://doodle.com/poll/';
 
-export async function fetchPollData(pollID: string): Promise<PollData> {
-  const response = await fetch(baseDoodleUrl + pollID);
-  const html = await response.text();
-  const matches = html.match(pollDataRegex);
+export async function fetchPollData(pollID: string, headers?: { [key: string]: string }): Promise<PollData> {
+  const options = {
+    method: 'GET'
+  };
+  if (headers) {
+    options['headers'] = headers;
+  }
+
+
+  const response = await fetch(baseDoodleUrl + pollID, options);
+  const html: string = await response.text();
+  const matches = pollDataRegex.exec(html);
   if (matches.length === 0) {
     throw new Error('Cannot match poll data');
   }
 
   try {
-    const pollData = JSON.parse(matches[0]);
-    return pollData;
+    const pollData = JSON.parse(matches[1]);
+    return pollData.poll;
   } catch (error) {
     throw new Error('Cannot parse poll data');
   }
